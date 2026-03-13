14b4639db6ce3aeead85072dd00479b4be9065e8
sydykovadilet909-coder <sydykovadilet909@gmail.com>
2026-03-13 17:39:48 +0600

    Add files via upload

---
+++ js.js
@@ -0,0 +1,32 @@
+const nome = "Adilet"
+let age = 25
+let online = false
+let user = {
+    name: "Adilet",
+    age: 25,
+    online: false
+}
+
+
+console.log(name, typeof age)
+console.log(age, typeof online)
+console.log(online, typeof user)
+console.log(user, typeof user)
+
+
+
+let phone = {
+    brand: "Samsung",
+    model: "Galaxy S24",
+    price: 50000
+}
+
+console.log(`Телефон: ${phone.brand} ${phone.model}, цена: ${phone.price} сом`)
+
+let book = {
+    title: "мастер и маргарита",
+    author: "Мичаил Булгаков",
+    year: 1967
+}
+
+
